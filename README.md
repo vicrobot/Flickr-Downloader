bd31ee4535d650bbfcd642ab2ebbcd9910b80f3c
Vicrobot <chaosplusorder@gmail.com>
2019-02-22 14:04:53 -0500

    Do seperate Folder creation

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Linux only.
   
 Firstly get your *API KEYS* AND *SECRET KEY*  from [here](https://www.flickr.com/services/api/keys/)
 
-Give the exact user profile name and get public photos of that user downloaded.
+Give user name(display name) and get public photos of that user downloaded.
 
 Install requirements.txt:-
 
--- flickr.py
+++ flickr.py
@@ -3,9 +3,11 @@ import flickr_api
 import os
 
 # Flickr api access key 
-flickr=flickrapi.FlickrAPI('YOUR API KEY HERE', 'YOUR API SECRET HERE')
-flickr_api.set_keys(api_key = "YOUR API KEY HERE", api_secret = "YOUR API SECRET HERE")
-user_id_val = flickr_api.Person.findByUserName(input('Give user exact name as on profile of flickr:- \n').rstrip()).id
+api_key_val = input('Give your API key  ').rstrip()
+secret_key_val = input('Give your API secret  ').rstrip()
+flickr=flickrapi.FlickrAPI(api_key_val, secret_key_val)
+flickr_api.set_keys(api_key = api_key_val, api_secret = secret_key_val)
+user_id_val = flickr_api.Person.findByUserName(input('Give user name:-  \n').rstrip()).id
 
 
 def url_list_maker(uiv):
@@ -20,6 +22,8 @@ def url_list_maker(uiv):
 
     
 urls = url_list_maker(user_id_val)
+os.mkdir('Flickr_Imgs')
+os.chdir('Flickr_Imgs')
 for i in urls:
     os.system('wget {}'.format(i)) # wget command thus linux restriction
  
