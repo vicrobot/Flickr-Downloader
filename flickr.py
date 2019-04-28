a261b4678db00213a6eedaa2d632351dff630efc
Vicrobot <chaosplusorder@gmail.com>
2019-04-28 14:26:50 -0400

    Modify Error handelings

--- flickr.py
+++ flickr.py
@@ -35,7 +35,7 @@ try:
         lines = [i.rstrip() for i in var.readlines() if len(i) ]
 except FileNotFoundError:
     with open('logs', 'w+') as  var:
-        lines = [i.rstrip() for i in var.readlines() if len(i) ]
+        lines = []
 bool_contain = -1
 bool_ask_old = 0
 dict_ids = {}
@@ -49,11 +49,6 @@ for line in lines:
     if 'id2' in line:
         bool_contain += 1
         dict_ids['id2'] = ''.join(line.split(' ')[1:])
-if bool_contain == 1:
-    flickr_api.set_keys(api_key = dict_ids['id1'], api_secret = dict_ids['id2'])
-    try: flickr_api.Person.findByUserName('vicro_bot').id
-    except flickr_api.flickrerrors.FlickrAPIError:
-        bool_contain = 0
 
 if bool_contain == 1:
     inp_ask_old = input('Use previously saved keys?(Yes or No)').rstrip().lower()
@@ -63,8 +58,18 @@ if bool_contain == 1:
         secret_key_val = dict_ids['id2']
         #print(secret_key_val)
 else:
-    api_key_val = input('Give your API key  ').rstrip()
-    secret_key_val = input('Give your API secret  ').rstrip()
+    while 1:
+        tjc = 1
+        api_key_val = input('Give your API key  ').rstrip()
+        secret_key_val = input('Give your API secret  ').rstrip()
+    
+        flickr_api.set_keys(api_key = api_key_val, api_secret = secret_key_val)
+        try: flickr_api.Person.findByUserName('vicro_bot').id
+        except flickr_api.flickrerrors.FlickrAPIError:
+            print("Wrong Keys!!", "Try again")
+            tjc = 0
+        if tjc: break
+    
     writable = ['id1 {}\n'.format(api_key_val), 'id2 {}\n'.format(secret_key_val)]
     with open('logs', 'w+') as var:
         var.writelines(writable)
