3c0768ac6e5aaaa10c2f88bbba17e93a4f9c4927
Vicrobot <chaosplusorder@gmail.com>
2019-09-27 09:00:23 -0400

    general working append

--- flickr_beta.py
+++ flickr_beta.py
@@ -1,8 +1,7 @@
 import flickrapi
 import flickr_api
 import urllib.request
-import os
-import sys
+import os, sys
 
 
 #######################
@@ -11,6 +10,9 @@ import sys
 
 incomplete and complete status too.
 urls' completion and incompletion too.
+
+Its been a lot of if else, we need GUI.
+All inputs should be from a particular class and their exception handeling.
 """
 #######################
 
@@ -20,6 +22,10 @@ if __name__ != "__main__":
 
 #functions
 def url_list_maker(uiv):
+    """
+    Returns a list of urls of all public photos a user has uploaded on flickr.
+    User is identified through user id value(uiv) in argument.
+    """
     count = 0
     photos = flickr.walk_user(user_id = uiv, per_page = 100, extras = 'url_o')
     url_list = []
@@ -30,6 +36,9 @@ def url_list_maker(uiv):
     return url_list
 
 def mkname(name):
+    """
+    Returns a possible non-conflicting name able to be created in current directory.
+    """
     num = 0
     name = str(name)
     new_n = name[:]
@@ -39,14 +48,46 @@ def mkname(name):
         num += 1
         new_n = name + str(num)
     return(new_n, old_n)
-    
+
 def checkIds(akv, skv, print_M = 0):
+    """
+    Checks whether the provided keys are correct or not.
+    akv is access key, akv is secret key.
+    print_M is used for showing a message if keys were wrong.
+    """
     flickr_api.set_keys(api_key = akv, api_secret = skv)
     try: flickr_api.Person.findByUserName('vicro_bot').id
     except flickr_api.flickrerrors.FlickrAPIError:
         if print_M: print("Wrong Keys!!", "Try again")
         return 0
     return 1
+
+def download(urls, filename, choice = 0, imagecount = 0):
+    """
+    Downloads files from url and uses filename as starting name for files.
+    choice : it is the way of accessing photos(like through tags, user name etc).
+    """
+    if choice == 1:
+        with open('.temp-logs', 'r+') as var: t_lines = var.readlines()
+    counter, b = 0, 0
+    var = 100.0/(len(urls)*1.0)
+    print('Downloading ... {:05}%'.format(int(counter)), end = '', flush = True)
+    for i in urls:
+        try: urllib.request.urlretrieve( i, '{1}{0}'.format(imagecount, filename[0]))
+        except KeyboardInterrupt:
+            if choice == 1:
+                #only for choice == 1 since no need to store urls for search results as they vary.
+                with open('.temp-logs', 'w+') as var:
+                    var.write(t_lines[0])
+                    var.write('imgC {}'.format(imagecount))
+                print('\nAbort')
+                sys.exit()
+            else: print('\nAbort'); sys.exit()
+        counter += var
+        print('\b'*6, end = '', flush = True)
+        imagecount += 1
+        print('{:05}'.format(counter)[:5]+'%', end = '', flush = True)
+    print('\nDone')
 
 #reading logs
 try:
@@ -71,7 +112,7 @@ for line in lines:
 
 if bool_contain == 1: bool_contain = checkIds(dict_ids['id1'],dict_ids['id2'])
 if bool_contain == 1:
-    inp_ask_old = input('Use previously saved keys?(Yes or No)').rstrip().lower()
+    inp_ask_old = input('Use previously saved keys?(Yes or No): ').rstrip().lower()
     if inp_ask_old == 'yes':
         bool_ask_old = 1
         api_key_val = dict_ids['id1']
@@ -91,53 +132,67 @@ if not bool_ask_old:
 #some globals' setup
 flickr=flickrapi.FlickrAPI(api_key_val, secret_key_val)
 flickr_api.set_keys(api_key = api_key_val, api_secret = secret_key_val)
-user_name = input('Give user name:-  \n').rstrip()
-user_id_val = flickr_api.Person.findByUserName(user_name).id
 
+types_uses = ['search through tags list', 'search through user name'] #more to be appended like a particular photoset &c
+choice = int(input('choose 0 for first element, 1 for second for\n{}\n'.format(types_uses)).rstrip())
 
+if choice == 1:
+    user_name = input('Give user name:-  \n').rstrip()
+    user_id_val = flickr_api.Person.findByUserName(user_name).id
 
-#directory work
-new_dir, old_dir = mkname('Flickr_Imgs_{}'.format('_'.join(user_name.split(' '))))
-if not os.path.exists(old_dir):
-    os.mkdir(new_dir)
-    os.chdir(new_dir)
-else: os.chdir(old_dir)
+    #directory work
+    new_dir, old_dir = mkname('Flickr_Imgs_{}'.format('_'.join(user_name.split(' '))))
+    if not os.path.exists(old_dir):
+        os.mkdir(new_dir)
+        os.chdir(new_dir)
+    else: os.chdir(old_dir)
 
-if not os.path.exists('.temp-logs'):
     imagecount = 0
-    with open('.temp-logs', 'w+') as var:
-        urls = url_list_maker(user_id_val)        ###### need to write something like yes! this has all urls and etc ##########
-        var.write(str(urls))
-        var.write('\n')
-else:
-    with open('.temp-logs', 'r+') as var:
-        lines_urls_lgs = var.readlines()
-    susp_int= lines_urls_lgs[1].rstrip()[5:]
-    if susp_int: imagecount = int(susp_int) -1
-    urls = eval(lines_urls_lgs[0].rstrip())[imagecount :]  #sometimes 1 image overridely get downloaded, toPreventLossOfUnperdictedIntruption
-
-
-# terminal show
-with open('.temp-logs', 'r+') as var: t_lines = var.readlines()
-counter = 0
-var = 100.0/(len(urls)*1.0)
-print('Downloading ... {:05}%'.format(int(counter)), end = '', flush = True)
-b = 0
-for i in urls:
-    try: urllib.request.urlretrieve( i, '{1}{0}'.format(imagecount, user_name[:1]))
-    except KeyboardInterrupt:
+    if not os.path.exists('.temp-logs'):
         with open('.temp-logs', 'w+') as var:
-            var.write(t_lines[0])
-            var.write('imgC {}'.format(imagecount))
-        print('\nAbort')
-        sys.exit()
-    except Exception: raise Exception
-    counter += var
-    print('\b'*6, end = '', flush = True)
-    imagecount += 1
-    print('{:05}'.format(counter)[:5]+'%', end = '', flush = True)
+            urls = url_list_maker(user_id_val)
+            var.write(str(urls))
+            var.write('\n')
+    else:
+        with open('.temp-logs', 'r+') as var:
+            lines_urls_lgs = var.readlines()
+        susp_int= lines_urls_lgs[1].rstrip()[5:]
+        if susp_int: imagecount = int(susp_int) -1
+        urls = eval(lines_urls_lgs[0].rstrip())[imagecount :]
+    download(urls, user_name, choice = 1, imagecount = imagecount)
+elif choice == 0:
+    bool_broad = int(input('You wanna search broad category or strict in tagging?\
+    (1 for former/prior, 0 for later);\n').rstrip())
+    text = input("Give a general text for search: ").strip()
+    if bool_broad == 1:
+        t = flickr.tags.getRelated(api_key = api_key_val, tag = input('give the tag name: ').rstrip())
+        tags = [[j.text for j in i] for i in t][0]
+        searched_elem = flickr.photos.search(api_key = api_key_val, tags = tags, 
+        text = text, accuracy = 1, safe_search = 1, content_type = 1, extras = 'url_o') #there is media argument, per_page and page too. GUI handling needed.
+    else:
+        tags = [i.strip() for i in input('give tags separated by comma: ').rstrip().split(',')]
+        searched_elem = flickr.photos.search(api_key = api_key_val, tags = tags,
+        text = text, accuracy = 1, safe_search = 1, content_type = 1, extras = 'url_o', 
+        per_page = int(input('how many images(max 500): ').rstrip()))
+    photo_elems = [[j for j in i] for i in searched_elem][0]
+    url_list = []
+    for p in photo_elems:
+        try:
+            dict_ = p.attrib
+            md = flickr.photos.getSizes(api_key = api_key_val, photo_id = dict_['id'])
+            t1 = [[j.attrib['source'] for j in i][-1] for i in md][0]
+            print(t1)
+            url_list.append(t1)
+        except: print('Error occured in retrieving url,', 'Ignoring')
+    #directory work
+    new_dir, old_dir = mkname('Flickr_Imgs_{}'.format('_'.join(text.split(' '))))
+    if not os.path.exists(old_dir):
+        os.mkdir(new_dir)
+        os.chdir(new_dir)
+    else: os.chdir(old_dir)
+    download(url_list, text, choice = 0)
 
-with open('.temp-logs', 'w+') as var:
-    var.write(t_lines[0])
-    var.write('imgC {}'.format(imagecount))    #Prob assumed that interuptn wil be at except block but 've2fix
-print('\nDone')
+
+#flickr.photos.getSizes was helpful for retrieving urls from photo ids. 
+# photo elems have attrib attribute having a dictionary of data about the photo's source etc.
+#An album is called photoset in flickr
